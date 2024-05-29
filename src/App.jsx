d1d6e48631bc4b23f89da93e554c25b891a4d13d
HoangHaix86 <hai.hoang.2762@gmail.com>
2024-05-29 20:26:49 +0700

    feat: create form register

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,17 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+const Register = () => {
+  return <>
+    <h3>Register</h3>
+    <form action="">
+      <input type="text" name="username" id="username" />
+      <input type="password" name="password" id="password" />
+      <button type='submit'>Login</button>
+    </form>
+  </>
+}
+
 function App() {
   const [count, setCount] = useState(0)
 
